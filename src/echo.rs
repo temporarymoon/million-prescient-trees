391b13c4d317ff9030eedb0018b187f09815591f
Matei Adriel <rafaeladriel11@gmail.com>
2022-11-20 04:23:26 +0100

    Started to implement hashing for info sets

--- src/echo.rs
+++ src/echo.rs
@@ -1,8 +1,18 @@
 #![allow(dead_code)]
 use flagset::{flags, FlagSet};
 
+#[derive(Clone, Copy)]
+pub enum Battlefield {
+    Mountain,
+    Glade,
+    Urban,
+    LastStrand,
+    Night,
+    Plains,
+}
+
 flags! {
-    pub enum Creature:u8 {
+    pub enum Creature:u16 {
         Wall,
         Seer,
         Rogue,
@@ -16,16 +26,7 @@ flags! {
         Monarch,
     }
 
-    pub enum Battlefield:u8 {
-        Mountain,
-        Glade,
-        Urban,
-        LastStrand,
-        Night,
-        Plains,
-    }
-
-    pub enum Eddict:u8 {
+    pub enum Edict:u8 {
         Sabotage,
         Gambit,
         Ambush,
@@ -36,7 +37,7 @@ flags! {
 
 use Battlefield::*;
 use Creature::*;
-use Eddict::*;
+// use Edict::*;
 
 impl Creature {
     // Strength of given creature (top-left of the card)
@@ -99,33 +100,33 @@ flags! {
 }
 
 type CreatureSet = FlagSet<Creature>;
-type EddictSet = FlagSet<Eddict>;
+type EdictSet = FlagSet<Edict>;
 type PlayerStatusEffects = FlagSet<PlayerStatusEffect>;
 type GlobalStatusEffects = FlagSet<GlobalStatusEffect>;
 
 // State involving only one of the players
 pub struct PlayerState {
-    cards: CreatureSet,
-    eddicts: EddictSet,
+    creatures: CreatureSet,
+    edicts: EdictSet,
     effects: PlayerStatusEffects,
 }
 
 // What a player "knows" about it's opponent
 pub struct HiddenPlayerState {
-    // The eddicts a player has in hand are fully determined by:
-    //    {set of eddicts} - {eddicts previously played}
+    // The edicts a player has in hand are fully determined by:
+    //    {set of edicts} - {edicts previously played}
     //
     // The reason we cannot do the same with creatures is
     // the existence of the overseer - a card which neither player
     // has been given. This means a player doesn't know what the
     // "full set of creatures the opponent has been given" is.
-    eddicts: EddictSet,
+    edicts: EdictSet,
     effects: PlayerStatusEffects,
 }
 
 // Choice made by one of the players in the main phase
 pub struct MainPhaseChoice {
-    eddict: Eddict,
+    edict: Edict,
     // The player is only allowed to play two creatures
     // if the "seer" status effect is active
     creatures: (Creature, Option<Creature>),
@@ -140,11 +141,11 @@ pub struct SabotagePhaseChoices(Option<Creature>, Option<Creature>);
 // A decision one of the players has to take
 pub enum TurnPhase {
     // Player (1/2) must pick one or two (if the seer status effect is active)
-    // creatures and an eddict to play
+    // creatures and an edict to play
     Main1,
     // holds the choice made in Main1
     Main2(MainPhaseChoice),
-    // The sabotage eddict gets resolved
+    // The sabotage edict gets resolved
     // (the player must make a prediction for what the opponent has played)
     // Holds the choices made in Main1/2
     SabotagePhase1(MainPhaseChoices),
@@ -192,7 +193,7 @@ pub struct GameState {
 // Main differences are:
 // - opponent's state is hidden
 // - the identity of the overseer is hidden
-pub struct InfoSet {
+pub struct InfoSet<'a> {
     // The player only has full information about themselves!
     player_states: (PlayerState, HiddenPlayerState),
     // The remaining fields have the same meaning
@@ -200,5 +201,67 @@ pub struct InfoSet {
     score: i8,
     graveyard: CreatureSet,
     effects: GlobalStatusEffects,
-    battlefields: Vec<Battlefield>,
+    battlefields: &'a Vec<Battlefield>,
+}
+
+impl PlayerState {
+    // Layout (20 bits):
+    // - [0-11] - creatures
+    // - [11-16] - edicts
+    // - [16-20] - effects
+    pub fn hash(&self) -> u64 {
+        let creatures = self.creatures.bits() as u64;
+        let edicts = self.edicts.bits() as u64;
+        let effects = self.effects.bits() as u64;
+        (effects << 16) | (edicts << 11) | creatures
+    }
+}
+
+impl HiddenPlayerState {
+    // Layout (9 bits):
+    // - [0-5] - edicts
+    // - [5-9] - effects
+    pub fn hash(&self) -> u64 {
+        let edicts = self.edicts.bits() as u64;
+        let effects = self.effects.bits() as u64;
+        (effects << 5) | edicts
+    }
+}
+
+impl<'a> InfoSet<'a> {
+    // 4 battlefields or less
+    // 5 values each
+    // => 5^4 = 625
+    // => < 2^10
+    // => 10 bits
+    fn hash_battlefields(&self) -> u64 {
+        let mut result = 0;
+        for battlefield in self.battlefields {
+            result = result * 5 + *battlefield as u64
+        }
+        return result;
+    }
+
+    // Layout:
+    // - [0-20] - Player 1 state
+    // - [20-29] - Player 2 state
+    // - [29-37] - Score
+    // - [37-48] - Graveyard
+    // - [48-49] - Effects
+    // - [49-59] - Battlefields
+    pub fn hash(&self) -> u64 {
+        let player1 = self.player_states.0.hash();
+        let player2 = self.player_states.1.hash();
+        let score = ((self.score as i64) + 128) as u64;
+        let graveryard = self.graveyard.bits() as u64;
+        let effects = self.effects.bits() as u64;
+        let battlefields = self.hash_battlefields();
+
+        (battlefields << 49)
+            | (effects << 48)
+            | (graveryard << 37)
+            | (score << 29)
+            | (player2 << 20)
+            | player1
+    }
 }
